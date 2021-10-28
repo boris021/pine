70173e760e7b0c9865274c54b25d6c61c0ff222e
boris <boris021@mail.ru>
2021-10-28 23:49:37 +0500

    fix

--- #src/js/files/functions.js
+++ #src/js/files/functions.js
@@ -526,3 +526,25 @@ animate({
 			Element.prototype.msMatchesSelector;
 	}
 })();
+
+// кнопка на вверх
+document.addEventListener('DOMContentLoaded', () => {
+
+	let toTopBtn = document.querySelector('.to-up');
+
+	window.onscroll = function () {
+		if (window.pageYOffset > 580) {
+			toTopBtn.style.display = 'block'
+		} else {
+			toTopBtn.style.display = 'none'
+		}
+	}
+
+	// плавный скролл наверх 
+	toTopBtn.addEventListener('click', function () {
+		window.scrollBy({
+			top: -document.documentElement.scrollHeight,
+			behavior: 'smooth'
+		});
+	});
+});
